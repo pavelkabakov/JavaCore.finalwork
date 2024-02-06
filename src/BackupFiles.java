1b43b0b888417fbc8423cf9af5d1ea7f7c9bc744
Павел Кабаков <pavelkabakov@gmail.com>
2024-02-06 21:17:11 +0300

    init

---
+++ src/BackupFiles.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class BackupFiles {
+
+    public static void main(String[] args) {
+        String sourceDirectory = "./src/testdir";
+
+        // Создаем новую папку для резервной копии
+        String backupDirectory = "./src/backup";
+        File backupDir = new File(backupDirectory);
+        backupDir.mkdir();
+
+        // Получаем список файлов в исходной директории
+        File sourceDir = new File(sourceDirectory);
+        File[] files = sourceDir.listFiles();
+
+        if (files != null) {
+            // Копируем каждый файл в новую директорию
+            for (File file : files) {
+                if (file.isFile()) {
+                    try {
+                        copyFile(file, new File(backupDirectory + "/" + file.getName()));
+                        System.out.println("Скопирован файл: " + file.getName());
+                    } catch (IOException e) {
+                        System.err.println("Ошибка при копировании файла " + file.getName());
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            System.out.println("Резервная копия завершена.");
+        } else {
+            System.err.println("Неверный путь к директории.");
+        }
+    }
+
+    private static void copyFile(File source, File destination) throws IOException {
+        Files.copy(source.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+}
